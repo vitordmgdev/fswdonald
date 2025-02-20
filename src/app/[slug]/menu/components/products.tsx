1a6a589e9412e898480a34be2c53a6d4d9926d3f
vitor.domingues.dev <123842676+vitordmgdev@users.noreply.github.com>
2025-02-20 11:37:55 -0300

    feat: add restaurant menu page with categories and products

---
+++ src/app/[slug]/menu/components/products.tsx
@@ -0,0 +1,44 @@
+import { Product } from "@prisma/client";
+import Image from "next/image";
+import Link from "next/link";
+
+interface ProductProps {
+    products: Product[]
+}
+
+const Products = ({ products }: ProductProps) => {
+    return ( 
+        <div className="space-y-3 px-5 py-3">
+            {
+                products.map((product) => (
+                    <Link key={product.id} href="/" className="flex items-center justify-between gap-10 py-3 border-b">
+                        <div>
+                            <h3 className="text-sm font-medium">
+                                {product.name}
+                            </h3>
+                            <p className="line-clamp-2 text-sm text-muted-foreground">
+                                {product.description}
+                            </p>
+                            <p className="pt-3 text-sm font-semibold">
+                                {new Intl.NumberFormat("pt-BR", {
+                                    style: 'currency',
+                                    currency: 'BRL'
+                                }).format(product.price)}
+                            </p>
+                        </div>
+                        <div className="relative min-h-[82px] min-w-[120px]">
+                            <Image 
+                            src={product.imageUrl} 
+                            alt={product.name}
+                            fill
+                            className="object-contain"
+                            />
+                        </div>
+                    </Link>
+                ))
+            }
+        </div>
+     );
+}
+ 
+export default Products;
